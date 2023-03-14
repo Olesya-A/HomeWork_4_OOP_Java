f4cdd1fd0eb5a312cc81befc4fb304b6d9218160
Olesya <lesusha1@yandex.ru>
2023-03-14 19:21:17 +0300

    added box

---
+++ Box.java
@@ -0,0 +1,28 @@
+import java.util.ArrayList;
+import java.util.List;
+
+public class Box<F extends Fruit> {
+
+    private final List<F> fruitBox = new ArrayList<>();
+
+    // Нужно реализовать хранение фруктов в коробке Box
+
+    public void add(F fruit) {
+        fruitBox.add(fruit);
+    }
+
+    public double getBoxWeight() {
+        double fruitsWeight = 0;
+        for (Fruit fruit : fruitBox) {
+            fruitsWeight += fruit.getWeight();
+        }
+        return fruitsWeight;
+    }
+
+    public void moveTo(Box<? super F> anotherBox) {
+        for (int i = 0; i < fruitBox.size(); i++) {
+            anotherBox.add(fruitBox.get(i));
+        }
+        fruitBox.clear(); 
+    }
+}
